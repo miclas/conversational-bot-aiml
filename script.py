72d089b419162eb6133a868b673089363b0dd710
Michał Łaski <michal.r.laski@gmail.com>
2020-05-28 23:29:00 +0200

    bot v.1.3'

---
+++ script.py
@@ -0,0 +1,9 @@
+import aiml
+ 
+k = aiml.Kernel()
+k.learn("data.aiml")
+print("")
+print("Klient wchodzi do sklepu z obuwiem i podchodzi do obslugi, aby sie przywitac")
+print("")
+while True:
+    print(k.respond(raw_input("> ")))
